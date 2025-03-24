d7f78ec992e68355cb7a49ba72430c8898a38986
engsr6982 <pwxg286704@163.com>
2025-03-24 13:21:18 +0800

    feat: add subland config

--- include/pland/Config.h
+++ include/pland/Config.h
@@ -30,7 +30,7 @@ struct Config {
         int  drawRange{64};           // 绘制 x 范围内的领地
 
         struct {
-            bool   enabled{true};                               // 是否启用
+            bool   enabled{false};                              // 是否启用
             int    maxNested{5};                                // 最大嵌套层数(默认5，最大16)
             int    minSpacing{8};                               // 子领地之间的最小间距
             int    maxSubLand{6};                               // 每个领地的最大子领地数量
