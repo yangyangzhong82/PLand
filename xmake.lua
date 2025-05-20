00d5be4af119922a33a74f3f28bc9d563602a7e9
engsr6982 <109733049+engsr6982@users.noreply.github.com>
2025-05-20 11:27:32 +0800

    chore: adapt levilamian v1.2.0_rc.2

--- xmake.lua
+++ xmake.lua
@@ -9,7 +9,7 @@ add_repositories("OTOTYAN https://github.com/OEOTYAN/xmake-repo.git")
 -- add_requires("levilamina develop") to use develop version
 -- please note that you should add bdslibrary yourself if using dev version
 if is_config("target_type", "server") then
-    add_requires("levilamina 1.2.0-rc.1", {configs = {target_type = "server"}})
+    add_requires("levilamina 1.2.0-rc.2", {configs = {target_type = "server"}})
 else
     add_requires("levilamina 1.0.1", {configs = {target_type = "client"}})
 end
