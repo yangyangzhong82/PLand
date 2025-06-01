d4507d4e3b6d320104717c61f73c6e704932dc4e
engsr6982 <pwxg286704@163.com>
2025-06-01 21:24:25 +0800

    feat: add database version check and backup logic

--- include/pland/LandData.h
+++ include/pland/LandData.h
@@ -104,26 +104,27 @@ struct LandPermTable {
 };
 
 
-using LandData_sptr = std::shared_ptr<class LandData>; // 共享指针
-using LandData_wptr = std::weak_ptr<class LandData>;   // 弱指针
+using LandData_sptr           = std::shared_ptr<class LandData>; // 共享指针
+using LandData_wptr           = std::weak_ptr<class LandData>;   // 弱指针
+constexpr int LandDataVersion = 17;                              // 领地数据版本号
 class LandData {
 public:
-    int                version{17};         // 版本号
-    LandAABB           mPos;                // 领地对角坐标
-    LandPos            mTeleportPos;        // 领地传送坐标
-    LandID             mLandID{LandID(-1)}; // 领地唯一ID  (由 PLand::addLand() 时分配)
-    LandDimid          mLandDimid;          // 领地所在维度
-    bool               mIs3DLand;           // 是否为3D领地
-    LandPermTable      mLandPermTable;      // 领地权限
-    UUIDs              mLandOwner;          // 领地主人(默认UUID,其余情况看mOwnerDataIsXUID)
-    std::vector<UUIDs> mLandMembers;        // 领地成员
-    std::string        mLandName{"Unnamed territories"_tr()}; // 领地名称
-    std::string        mLandDescribe{"No description"_tr()};  // 领地描述
-    bool               mIsSaleing{false};                     // 是否正在出售
-    int                mSalePrice{0};                         // 出售价格
-    int                mOriginalBuyPrice{0};                  // 原始购买价格
-    bool               mIsConvertedLand{false}; // 是否为转换后的领地(其它插件创建的领地)
-    bool mOwnerDataIsXUID{false}; // 领地主人数据是否为XUID (如果为true，则主人上线自动转换为UUID)
+    int                 version{LandDataVersion};              // 版本号
+    LandAABB            mPos;                                  // 领地对角坐标
+    LandPos             mTeleportPos;                          // 领地传送坐标
+    LandID              mLandID{LandID(-1)};                   // 领地唯一ID  (由 PLand::addLand() 时分配)
+    LandDimid           mLandDimid;                            // 领地所在维度
+    bool                mIs3DLand;                             // 是否为3D领地
+    LandPermTable       mLandPermTable;                        // 领地权限
+    UUIDs               mLandOwner;                            // 领地主人(默认UUID,其余情况看mOwnerDataIsXUID)
+    std::vector<UUIDs>  mLandMembers;                          // 领地成员
+    std::string         mLandName{"Unnamed territories"_tr()}; // 领地名称
+    std::string         mLandDescribe{"No description"_tr()};  // 领地描述
+    bool                mIsSaleing{false};                     // 是否正在出售
+    int                 mSalePrice{0};                         // 出售价格
+    int                 mOriginalBuyPrice{0};                  // 原始购买价格
+    bool                mIsConvertedLand{false};               // 是否为转换后的领地(其它插件创建的领地)
+    bool                mOwnerDataIsXUID{false};   // 领地主人数据是否为XUID (如果为true，则主人上线自动转换为UUID)
     LandID              mParentLandID{LandID(-1)}; // 父领地ID
     std::vector<LandID> mSubLandIDs;               // 子领地ID
 
